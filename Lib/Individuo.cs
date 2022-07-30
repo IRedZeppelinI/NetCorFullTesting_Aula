4edf6661b8d4c133774ac59b1f4bf88952284f21
Sergio Fontes <sergio@computencial.com>
2022-07-30 10:44:12 +0100

    new project

---
+++ Lib/Individuo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib
+{
+    public class Individuo
+    {
+        public string Nome;
+        public string Apelido;
+        public DateTime DataNasc;
+    }
+}
